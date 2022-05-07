73498f135c81316a262d7ba8a4b7f0bbf52dc5e9
Arthur @Refzlund <arthur@refzlund.com>
2022-05-07 12:56:51 +0200

    🔧 querySpread accepts untyped URLSearchParams

--- lib/utility.ts
+++ lib/utility.ts
@@ -51,9 +51,9 @@ type AllStrings<T extends Record<any, any>> = {
  * '"2022-05-06T22:15:11.244Z"' => new Date("2022-05-06T22:15:11.244Z") // Has quotes around the ISO-string (from `new Date()`)
  * ```
 */
-export function querySpread<T extends QueryGetter<any>>(searchParams: T): DetermineValueTypes<InferQueryGetter<T>['query']>
-export function querySpread<T extends QueryGetter<any>, B extends boolean>(searchParams: T, formatTypes: B): B extends true | null | undefined ? DetermineValueTypes<InferQueryGetter<T>['query']> : AllStrings<InferQueryGetter<T>['query']>
-export function querySpread<T extends QueryGetter<any>, B extends boolean>(searchParams: T, formatTypes?: B) {
+export function querySpread<T extends QueryGetter<any> | URLSearchParams>(searchParams: T): T extends QueryGetter<any> ? DetermineValueTypes<InferQueryGetter<T>['query']> : Record<any, any>
+export function querySpread<T extends QueryGetter<any> | URLSearchParams, B extends boolean>(searchParams: T, formatTypes: B): T extends QueryGetter<any> ? B extends true | null | undefined ? DetermineValueTypes<InferQueryGetter<T>['query']> : AllStrings<InferQueryGetter<T>['query']> : Record<any, any>
+export function querySpread<T extends QueryGetter<any> | URLSearchParams, B extends boolean>(searchParams: T, formatTypes?: B) {
 	let obj = {}
 	// FIXME: Not sure why .entries() isn't a part of URLSearchParams
 	// @ts-ignore
