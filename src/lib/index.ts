d0f82df78d66792aeebda105ad7032e83dd81bd4
Arthur @Refzlund <arthur@refzlund.com>
2022-09-24 22:50:26 +0200

    `AnyAPI` type for parameters which accepts any SvelteKit server event

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,9 @@
+import type { RequestEvent, ServerLoadEvent } from '@sveltejs/kit'
+import type { API } from './types/zeroapi.js'
+
 export type { API, APIInputs } from './types/zeroapi.js'
 export { querySpread } from './querySpread.js'
 export { watch as watchAPI } from './watch/index.js'
 export { err } from './error-handling.js'
+
+export type AnyAPI = API<any> | RequestEvent | ServerLoadEvent
