e220fe8586178f3cb01fb7387c5eff3c2ffc422f
Arthur @Refzlund <arthur@refzlund.com>
2023-04-12 02:08:41 +0200

    Removed exports const

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,6 @@
 import adapter from '@sveltejs/adapter-auto'
 import preprocess from 'svelte-preprocess'
 
-const exports = [
-	'index.ts',
-	'__temp.ts',
-	'http.ts',
-	'api/index.ts',
-	'watch/index.ts',
-	'types/zeroapi.d.ts',
-	'queryspread.ts',
-	'helpers.ts'
-]
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
