a950439a939c38ebca9c577aaa99bc5c58b2088b
Arthur @Refzlund <arthur@refzlund.com>
2023-04-19 18:19:29 +0200

    Final solution in `test.ts` for Generic support

--- src/routes/test.ts
+++ src/routes/test.ts
@@ -1,4 +1,6 @@
 import type { API } from '$dist'
+import { Ok } from '$dist/http'
+import type { EP, Fetch, Z, R } from '$dist/z'
 
 interface Get {
 	query: {
@@ -7,36 +9,33 @@ interface Get {
 }
 
 interface Post {
-	body: {
+	body?: {
 		foo: string
 	}
 }
 
 const endpoint = {
 	GET: (event: API<Get>) => ({ test: 'test' }),
-	POST: <const Request extends Post>(event: API<Request>) => ({ requestBody: { } as Request['body'] })
+	POST: <const Request extends Post>(event: API<Request>) => {
+		return Ok({
+			body: {} as unknown as Request['body']
+		})
+	}
 }
-
-type InferAPI<T extends API> = T extends API<infer Input> ? Input : {}
 
 type GeneratedAPI = {
 	users: {
-		POST: <const T extends InferAPI<Parameters<typeof endpoint.POST>[0]>>(request: T) => typeof endpoint.POST<T>
-	}
+		POST: EP<typeof endpoint.POST> extends { body: any } | { query: any }
+			? <const T extends EP<typeof endpoint.POST>>(request: T, fetch?: Fetch) => R<typeof endpoint.POST<T>>
+			: <const T extends EP<typeof endpoint.POST>>(request?: T, fetch?: Fetch) => R<typeof endpoint.POST<T>>
+	} & Z<Omit<typeof endpoint, 'POST'>>
 }
 
 const api = {} as GeneratedAPI
 
 const test = api.users.POST({ body: { foo: 'yas' } })
-	// ^?
-	/*
-		const test: (event: API<{
-			readonly body: {
-				readonly foo: "yas";
-			};
-		}>) => {
-			requestBody: {
-				readonly foo: "yas";
-			};
-		}
-	*/
+	.Ok(res => {
+		res.body.foo
+				// ^? (property) foo: "yas"
+	})
+
