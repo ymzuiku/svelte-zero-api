fbe394e4dd9c5226b87fec806c0d29f913ed6754
Arthur @Refzlund <arthur@refzlund.com>
2023-04-21 20:15:14 +0200

    0.15.0 pipeline function🕺

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,8 +4,12 @@ import type { PageServerLoad } from './$types'
 
 export const load: PageServerLoad = async ({ parent, fetch }) => {
 
-	const response = await api.fo.POST({}, fetch).Ok(res => {
-		
+	const response = await api.fo.POST({
+		headers: {
+			'content-type': 'application/json'
+		}
+	}, fetch).Ok(res => {
+		console.log('POST from within the server')
 	})
 
 	return {
